ea1fe2ecb7abaffc985b8d958570bcc65f5bfc7b
lmcygnus <mloida591@gmail.com>
2024-06-13 15:09:57 -0400

    add caesarCipher function

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,1 +1,22 @@
-const characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k' 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+function findIndex(string) {
+  const array = string.split('');
+  const result = [];
+  for (const character of array) {
+    const index = characters.indexOf(character.toLowerCase());
+    result.push(index);
+  }
+  return result;
+}
+
+function caesarCipher(string, n) {
+  const result = [];
+  const indexes = findIndex(string);
+  for (const index of indexes) {
+    result.push(characters[index + n]);
+  }
+  return result;
+}
+
+console.log(caesarCipher('hello', 3));
